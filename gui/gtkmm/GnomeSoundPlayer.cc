b7221582f13f9e33407363cc145c53c52bb94d06
Raymond Penners <raymond@dotsphinx.com>
2002-10-01 20:22:16 +0000

    Sound
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@76 2b0e655b-6f86-497a-824e-95ef0e1e0d90

---
+++ gui/gtkmm/GnomeSoundPlayer.cc
@@ -0,0 +1,35 @@
+// GnomeSoundPlayer.cc --- Sound player
+//
+// Copyright (C) 2002 Rob Caelers & Raymond Penners
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+
+static const char rcsid[] = "$Id$";
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "GnomeSoundPlayer.hh"
+
+void
+GnomeSoundPlayer::destroy()
+{
+  delete this;
+}
+
+void
+GnomeSoundPlayer::play_sound(Sound snd)
+{
+}
+
