a1611d116e0663c371e0de87e4e8c02df3ec1853
Raymond Penners <raymond@dotsphinx.com>
2002-10-06 19:01:29 +0000

    thread
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@100 2b0e655b-6f86-497a-824e-95ef0e1e0d90

---
+++ arch/win32/Thread.hh
@@ -0,0 +1,47 @@
+// Thread.hh --- Thread class
+//
+// Copyright (C) 2002 Rob Caelers & Raymond Penners
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// $Id$
+//
+
+#ifndef THREAD_HH
+#define THREAD_HH
+
+#include <windows.h>
+#include "Runnable.hh"
+/*!
+ * Thread class.
+ */
+class Thread : public Runnable
+{
+public:
+  Thread(bool auto_delete = false);
+  virtual ~Thread();
+  
+  virtual void start();
+  void wait();
+  virtual void run();
+
+  static void sleep(long millis, int nanos = 0);
+  
+private:
+  static DWORD WINAPI thread_handler(LPVOID lpParameter);
+
+  HANDLE thread_handle;
+  bool auto_delete;
+};
+
+
+#endif // THREAD_HH
