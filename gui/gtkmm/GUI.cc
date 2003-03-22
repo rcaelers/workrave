c8178f7960265bfa47ddd9e6db752b0bf3dd6099
Rob Caelers <robc@krandor.nl>
2003-03-22 21:05:23 +0000

    win32 fix
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@393 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- gui/gtkmm/GUI.cc
+++ gui/gtkmm/GUI.cc
@@ -1,6 +1,6 @@
 // GUI.cc --- The WorkRave GUI
 //
-1// Copyright (C) 2001, 2002, 2003 Rob Caelers & Raymond Penners
+// Copyright (C) 2001, 2002, 2003 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
