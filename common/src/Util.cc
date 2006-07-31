547d9daaf846b06341f17a9dfb587d87a49f251a
Rob Caelers <robc@krandor.nl>
2006-07-31 21:20:40 +0000

    relative datadir
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@976 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- common/src/Util.cc
+++ common/src/Util.cc
@@ -44,7 +44,7 @@ extern "C" {
 HRESULT WINAPI SHGetSpecialFolderLocation(HWND,int,void**);
 BOOL WINAPI SHGetPathFromIDList(void *,LPSTR);
 VOID WINAPI CoTaskMemFree(PVOID);
-WINSHLWAPI BOOL WINAPI PathCanonicalize(LPSTR,LPCSTR);
+BOOL WINAPI PathCanonicalize(LPSTR,LPCSTR);
 #define CSIDL_APPDATA   26
 }
 // (end of hack)
