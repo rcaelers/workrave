d78db644d2f02bd2b334d7f262f45b12e26a27d3
Rob Caelers <robc@krandor.nl>
2007-02-12 22:41:44 +0000

    applet
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@1130 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- frontend/gtkmm/src/AppletWindow.cc
+++ frontend/gtkmm/src/AppletWindow.cc
@@ -1,6 +1,6 @@
 // AppletWindow.cc --- Applet info Window
 //
-// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 Rob Caelers & Raymond Penners
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -14,7 +14,6 @@
 // GNU General Public License for more details.
 //
 // TODO: release CORBA memory.
-// TODO: refactor. split into 4 classes.
 
 static const char rcsid[] = "$Id$";
 
