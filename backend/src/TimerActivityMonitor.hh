c5347e416ec3b4bf9e639aed57cbd9128e159ad0
Rob Caelers <robc@krandor.nl>
2007-02-19 22:57:16 +0000

    compile fix
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@1142 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- backend/src/TimerActivityMonitor.hh
+++ backend/src/TimerActivityMonitor.hh
@@ -1,6 +1,6 @@
 // TimerActivityMonitor.hh
 //
-// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 Rob Caelers <robc@krandor.org>
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007 Rob Caelers <robc@krandor.org>
 // All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -74,7 +74,7 @@ public:
         if (local_state != ACTIVITY_IDLE &&
             local_state != ACTIVITY_SUSPENDED)
           {
-            forced_idle = FALSE;
+            forced_idle = false;
           }
 
         TRACE_EXIT();
