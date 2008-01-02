1c6225a8ef9b31cb0b10500e062da48cab09b345
Rob Caelers <robc@krandor.nl>
2008-01-02 23:20:23 +0000

    osx add
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@1397 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- frontend/gtkmm/osx/make-image.sh
+++ frontend/gtkmm/osx/make-image.sh
@@ -18,7 +18,7 @@ rm -rf "$dmg_dir"
 mkdir "$dmg_dir"
 
 cp -rf Workrave.app "$dmg_dir"
-cp -f *webloc "$dmg_dir"
+#cp -f *webloc "$dmg_dir"
 ln -sf /Applications "$dmg_dir"
 
 mkdir "$dmg_dir/.background"
@@ -51,6 +51,7 @@ hdiutil detach "${DEV_NAME}"
 
 echo "Compressing disk image..."
 
+rm -f "${dmg_name}"
 hdiutil convert "${dmg_tmp_name}" -format UDZO -imagekey zlib-level=9 -o "${dmg_name}"
 rm -f "${dmg_tmp_name}"
 
@@ -60,8 +61,81 @@ rm -f "${dmg_tmp_name}"
 
 echo "adding EULA resources"
 
+echo "
+data 'LPic' (5000)
+{
+  // Default language ID, 0 = English
+  $\"0000\"
+  // Number of entries in list
+  $\"0001\"
+
+  // Entry 1
+  // Language ID, 0 = English
+  $\"0000\"
+  // Resource ID, 0 = STR#/TEXT/styl 5000
+  $\"0000\"
+  // Multibyte language, 0 = no
+  $\"0000\"
+};
+
+data 'styl' (5000, \"English\") {
+  // Number of styles following = 1
+  $\"0001\"
+
+  // Style 1.  This is used to display the first two lines in bold text.
+  // Start character = 0
+  $\"0000 0000\"
+  // Height = 16
+  $\"0010\"
+  // Ascent = 12
+  $\"000C\"
+  // Font family = 1024 (Lucida Grande)
+  $\"0400\"
+  // Style bitfield, 0x1=bold 0x2=italic 0x4=underline 0x8=outline
+  // 0x10=shadow 0x20=condensed 0x40=extended
+  $\"00\"
+  // Style, unused?
+  $\"02\"
+  // Size = 12 point
+  $\"000A\"
+  // Color, RGB
+  $\"0000 0000 0000\"
+};
+
+resource 'STR#' (5000, \"English\") {
+  {
+    // Language (unused?) = English
+    \"English\",
+    
+    // Agree
+    \"Agree\",
+
+    // Disagree
+    \"Disagree\",
+
+    // Print, ellipsis is 0xC9
+    \"Print...\",
+
+    // Save As, ellipsis is 0xC9
+    \"Save As...\",
+
+    // Descriptive text, curly quotes are 0xD2 and 0xD3
+    \"If you agree to the terms of this license agreement, click \`Agree\' to access the software.  If you \"
+    \"do not agree, press \`Disagree.\'\"
+  }
+};
+
+data 'TEXT' (5000, \"English\") {
+" > tmp.r
+
+sed -e 's/"/\\"/g' -e 's/\(.*\)$/"\1\\n"/g' ../../../COPYING >> tmp.r
+
+echo "
+};
+" >> tmp.r
+
 hdiutil unflatten "${dmg_name}"
-/Developer/Tools/ResMerger -a "${EULA_RSRC}" -o "${dmg_name}"
+/Developer/Tools/Rez /Developer/Headers/FlatCarbon/*.r tmp.r -a -o "${dmg_name}"
 hdiutil flatten "${dmg_name}"
 
 echo "Disk image done"
