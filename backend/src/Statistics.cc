982e11d0fb91223b0b10f42c3e0194f93bef2746
Rob Caelers <robc@krandor.nl>
2008-01-26 17:50:39 +0000

    bug #706
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@1426 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- backend/src/Statistics.cc
+++ backend/src/Statistics.cc
@@ -1,6 +1,6 @@
 // Statistics.cc
 //
-// Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 Rob Caelers & Raymond Penners
+// Copyright (C) 2002 - 2008 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -24,6 +24,7 @@ static const char rcsid[] = "$Id$";
 #endif
 
 
+#include <cstring>
 #include <sstream>
 #include <assert.h>
 #include <math.h>
