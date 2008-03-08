2f4e69b9f24bbc834f80d8bc04e6e71299ca4080
Rob Caelers <robc@krandor.nl>
2008-03-08 10:59:40 +0000

    typo
    git-svn-id: https://workrave.svn.sourceforge.net/svnroot/workrave/workrave/workrave/trunk@1456 2b0e655b-6f86-497a-824e-95ef0e1e0d90

--- frontend/common/src/kde/KdeSoundPlayer.cc
+++ frontend/common/src/kde/KdeSoundPlayer.cc
@@ -1,6 +1,6 @@
 // KdeSoundPlayer.cc --- Sound player
 //
-// Copyright (C) 2002, 2003, 2004, 2007 Rob Caelers & Raymond Penners
+// Copyright (C) 2002, 2003, 2004, 2007, 2008 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -48,7 +48,7 @@ KdeSoundPlayer::~KdeSoundPlayer()
 void
 KdeSoundPlayer::play_sound(Sound snd)
 {
-  char *map[] = {
+  const char *map[] = {
     "break_prelude",
     "break_ignored",
     "rest_break_started",
