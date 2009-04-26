55dba1b2855915209e41c557aeab3309c0733730
Rob Caelers <robc@krandor.org>
2009-04-26 19:07:07 +0200

    restore win32 tracing

--- frontend/gtkmm/src/main.cc
+++ frontend/gtkmm/src/main.cc
@@ -1,6 +1,6 @@
 // main.cc --- Main
 //
-// Copyright (C) 2001, 2002, 2003, 2006, 2007, 2008 Rob Caelers & Raymond Penners
+// Copyright (C) 2001, 2002, 2003, 2006, 2007, 2008, 2009 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -101,7 +101,7 @@ int WINAPI WinMain (HINSTANCE hInstance,
 #endif
 
 #ifndef NDEBUG 
-#ifdef PLATFORM_OS_WIN32_NATIVE
+      //#ifdef PLATFORM_OS_WIN32_NATIVE
 	AllocConsole();
 
 	FILE* hf_out = fopen("c:\\temp\\out", "w");
@@ -117,7 +117,7 @@ int WINAPI WinMain (HINSTANCE hInstance,
       setvbuf(hf_in, NULL, _IONBF, 128);
       *stdin = *hf_in;
     }
-#endif
+  //#endif
 #endif
 
       run(sizeof(argv)/sizeof(argv[0]), argv);
