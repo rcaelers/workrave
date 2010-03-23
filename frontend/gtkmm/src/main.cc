aa5611433c324cbc5ea5a09d462cacee4948dc8e
Rob Caelers <robc@krandor.org>
2010-03-24 00:12:04 +0100

    Fix compilation error
    Signed-off-by: Rob Caelers <robc@krandor.org>

--- frontend/gtkmm/src/main.cc
+++ frontend/gtkmm/src/main.cc
@@ -1,6 +1,6 @@
 // main.cc --- Main
 //
-// Copyright (C) 2001, 2002, 2003, 2006, 2007, 2008, 2009 Rob Caelers & Raymond Penners
+// Copyright (C) 2001, 2002, 2003, 2006, 2007, 2008, 2009, 2010 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -69,7 +69,7 @@ run(int argc, char **argv)
 }
 
 
-#if (!defined(PLATFORM_OS_WIN32))) // || (!defined(PLATFORM_OS_WIN32_NATIVE) && !defined(NDEBUG))
+#if (!defined(PLATFORM_OS_WIN32)) // || (!defined(PLATFORM_OS_WIN32_NATIVE) && !defined(NDEBUG))
 int
 main(int argc, char **argv)
 {
