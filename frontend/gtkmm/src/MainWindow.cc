bfb2206b998517cf6712672524b9321e32543398
Rob Caelers <robc@krandor.org>
2011-01-11 20:53:31 +0100

    Enable tray when main window is closed.
    Signed-off-by: Rob Caelers <robc@krandor.org>

--- frontend/gtkmm/src/MainWindow.cc
+++ frontend/gtkmm/src/MainWindow.cc
@@ -1,6 +1,6 @@
 // MainWindow.cc --- Main info Window
 //
-// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 Rob Caelers & Raymond Penners
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Rob Caelers & Raymond Penners
 // All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -373,6 +373,7 @@ MainWindow::close_window()
       iconify();
     }
 #endif
+  GUIConfig::set_trayicon_enabled(true);
   TRACE_EXIT();
 }
 
