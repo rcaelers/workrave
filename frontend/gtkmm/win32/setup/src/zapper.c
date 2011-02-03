7a38d8dc1cc1b16458f7d18b4d9c4557633455d3
Rob Caelers <robc@krandor.org>
2011-02-03 23:30:06 +0100

    Fix crash in Workrave Zapper

--- frontend/gtkmm/win32/setup/src/zapper.c
+++ frontend/gtkmm/win32/setup/src/zapper.c
@@ -1,11 +1,11 @@
 #include <windows.h>
 #include "zapper.h"
 
-typedef DWORD (__stdcall *PFNQUERYFULLPROCESSIMAGENAME)(HANDLE, DWORD, LPTSTR, PDWORD);
-typedef DWORD (__stdcall *PFNGETMODULEFILENAMEEX)(HANDLE, HMODULE, LPTSTR, DWORD);
+typedef DWORD (__stdcall *QUERYFULLPROCESSIMAGENAME)(HANDLE, DWORD, LPTSTR, PDWORD);
+typedef DWORD (__stdcall *GETMODULEFILENAMEEX)(HANDLE, HMODULE, LPTSTR, DWORD);
 
-static PFNQUERYFULLPROCESSIMAGENAME pfnQueryFullProcessImageName = NULL;
-static PFNGETMODULEFILENAMEEX pfnGetModuleFileNameEx = NULL;
+static QUERYFULLPROCESSIMAGENAME pfnQueryFullProcessImageName = NULL;
+static GETMODULEFILENAMEEX pfnGetModuleFileNameEx = NULL;
 
 static int success = FALSE;
 
@@ -26,12 +26,12 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, long lParam)
         {
           if (pfnQueryFullProcessImageName != NULL)
             {
-              DWORD buf_size = sizeof(buf) - 1;
+              DWORD buf_size = sizeof(buf);
               pfnQueryFullProcessImageName(process_handle, 0, buf, &buf_size);
             }
           else if (pfnGetModuleFileNameEx != NULL)
             {
-              pfnGetModuleFileNameEx(process_handle, NULL, buf, sizeof(buf) - 1);
+              pfnGetModuleFileNameEx(process_handle, NULL, buf, sizeof(buf));
             }
 
           CloseHandle(process_handle);
@@ -40,7 +40,7 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, long lParam)
       char *ptr = strrchr(buf, '\\');
       if (ptr != NULL && strcmp(ptr, "Workrave.exe"))
         {
-          PostMessage(hwnd,  WM_ENDSESSION, 1, 0);
+          PostMessage(hwnd, WM_ENDSESSION, 1, 0);
 
           success = TRUE;
           return FALSE;
@@ -55,20 +55,29 @@ int ZapWorkrave()
   HINSTANCE kernel32 = LoadLibrary("kernel32.dll");
   if (kernel32 != NULL)
     {
-      pfnQueryFullProcessImageName = (PFNQUERYFULLPROCESSIMAGENAME)GetProcAddress(kernel32, "QueryFullProcessImageNameA");
-      FreeLibrary(kernel32);
+      pfnQueryFullProcessImageName = (QUERYFULLPROCESSIMAGENAME)GetProcAddress(kernel32, "QueryFullProcessImageNameA");
     }
 
-  if (pfnQueryFullProcessImageName == NULL )
+  HINSTANCE psapi = NULL;
+  if (pfnQueryFullProcessImageName == NULL)
     {
-      HINSTANCE psapi = LoadLibrary("psapi.dll");
-      pfnGetModuleFileNameEx = (PFNGETMODULEFILENAMEEX)GetProcAddress(psapi, "GetModuleFileNameExA");
-      FreeLibrary(psapi);
+      psapi = LoadLibrary("psapi.dll");
+      pfnGetModuleFileNameEx = (GETMODULEFILENAMEEX)GetProcAddress(psapi, "GetModuleFileNameExA");
     }
 
   if (pfnQueryFullProcessImageName != NULL || pfnGetModuleFileNameEx != NULL)
     {
       EnumWindows(EnumWindowsProc, 0);
+    }
+
+  if (kernel32 != NULL)
+    {
+      FreeLibrary(kernel32);
+    }
+
+  if (psapi != NULL)
+    {
+      FreeLibrary(psapi);
     }
 
   return success;
