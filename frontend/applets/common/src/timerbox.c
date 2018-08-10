3706aff54aca854d42480c1fd5fd25f2adb7a576
Rob Caelers <rob.caelers@gmail.com>
2018-08-10 14:21:13 +0200

    Fix libworkrave exported symbols
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- frontend/applets/common/src/timerbox.c
+++ frontend/applets/common/src/timerbox.c
@@ -373,8 +373,8 @@ workrave_timerbox_update(WorkraveTimerbox *self, GtkImage *image)
 }
 
 /**
- * workrave_timerbox_draw: 
- * @self: a @WorkraveTimerbox 
+ * workrave_timerbox_draw:
+ * @self: a @WorkraveTimerbox
  * @cr: a @cairo_t where the timerbox will be drawn into
  *
  */
@@ -392,10 +392,10 @@ workrave_timerbox_draw(WorkraveTimerbox *self, cairo_t *cr)
 }
 
 /**
- * workrave_timerbox_get_width: 
- * @self: a @WorkraveTimerbox 
+ * workrave_timerbox_get_width:
+ * @self: a @WorkraveTimerbox
  *
- * Return value: (transfer none): The width of the @WorkraveTimerbox
+ * Return value: The width of the @WorkraveTimerbox
  *
  */
 int
@@ -412,7 +412,7 @@ workrave_timerbox_get_width(WorkraveTimerbox *self)
  * workrave_timerbox_get_height: 
  * @self: a @WorkraveTimerbox 
  *
- * Return value: (transfer none): The width of the @WorkraveTimerbox
+ * Return value: The width of the @WorkraveTimerbox
  *
  */
 int
