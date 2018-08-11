8aaae3bf10a1f40c467be52a0925aa79e33dced5
Rob Caelers <rob.caelers@gmail.com>
2018-08-11 21:39:52 +0200

    Update docker
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/docker/create.sh
+++ build/docker/create.sh
@@ -1,6 +1,7 @@
-docker build -t rcaelers/workrave-build-mingw-gtk           build/docker/mingw-gtk
-docker build -t rcaelers/workrave-build-mingw-qt5           build/docker/mingw-qt5
-docker build -t rcaelers/workrave-build-ubuntu-xenial       build/docker/ubuntu-xenial/
-docker build -t rcaelers/workrave-build-ubuntu-yakkety      build/docker/ubuntu-yakkety/
-docker build -t rcaelers/workrave-build-ubuntu-zesty        build/docker/ubuntu-zesty
-docker build -t rcaelers/workrave-build-ubuntu-zesty-plus   build/docker/ubuntu-zesty-plus/
+docker build -t rcaelers/workrave-build:mingw-gtk           build/docker/mingw-gtk
+docker build -t rcaelers/workrave-build:mingw-qt5           build/docker/mingw-qt5
+docker build -t rcaelers/workrave-build:ubuntu-xenial       build/docker/ubuntu-xenial/
+docker build -t rcaelers/workrave-build:ubuntu-zesty        build/docker/ubuntu-zesty
+docker build -t rcaelers/workrave-build:ubuntu-artful       build/docker/ubuntu-artful
+docker build -t rcaelers/workrave-build:ubuntu-bionic       build/docker/ubuntu-bionic
+docker build -t rcaelers/workrave-build:ubuntu-bionic-plus  build/docker/ubuntu-bionic-plus
