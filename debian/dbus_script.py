49a365f5bbf18355f03dd79fcb92fb08b3f87957
Rob Caelers <rob.caelers@gmail.com>
2018-09-01 21:29:57 +0200

    Initial commit
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

---
+++ debian/dbus_script.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import os
+import sys
+import pwd
+import dbus
+import dbus.decorators
+import dbus.glib
+import time
+import gobject
+
+class WorkraveDBus:
+
+    def __init__(self):
+
+        bus = dbus.SessionBus()
+        obj = bus.get_object("org.workrave.Workrave", "/org/workrave/Workrave/Core")
+
+        workrave = dbus.Interface(obj, "org.workrave.CoreInterface")
+
+        workrave.connect_to_signal("MicrobreakChanged",
+                                   self.microbreak_signal, sender_keyword='sender')
+        workrave.connect_to_signal("RestbreakChanged",
+                                   self.restbreak_signal, sender_keyword='sender')
+        workrave.connect_to_signal("DailylimitChanged",
+                                   self.dailylimit_signal, sender_keyword='sender')
+
+    def microbreak_signal(self, progress, sender=None):
+        self.break_signal("microbreak", progress)
+
+    def restbreak_signal(self, progress, sender=None):
+        self.break_signal("restbreak", progress)
+
+    def dailylimit_signal(self, progress, sender=None):
+        self.break_signal("dailylimit", progress)
+
+    def break_signal(self, breakid, progress, sender=None):
+
+        if progress == "prelude":
+            print "Break warning %s" % breakid
+        elif progress == "break":
+            print "Break %s started" % breakid
+        elif progress == "none":
+            print "Break %s idle" % breakid
+        else:
+            print "Unknown progress for %s: %s" % (breakid, progress)
+
+if __name__ == '__main__':
+
+    d = WorkraveDBus()
+
+    loop = gobject.MainLoop()
+    loop.run()
