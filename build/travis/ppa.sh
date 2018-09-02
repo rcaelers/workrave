6cdcff782d73903691e243984a85ac416aca291a
Rob Caelers <rob.caelers@gmail.com>
2018-09-02 21:46:11 +0200

    Now, pass secret key to docker container...
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/travis/ppa.sh
+++ build/travis/ppa.sh
@@ -2,6 +2,7 @@
 
 export DEBEMAIL="robc@krandor.org"
 
+SOURCE_DIR=/workspace/source
 BUILD_DIR=/workspace/source/_dist/build
 DEBIAN_PACKAGING_DIR=/workspace/source/_dist/debian-packaging
 
@@ -9,7 +10,7 @@ rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 mkdir -p ${DEBIAN_PACKAGING_DIR}
 
-cd /workspace/source
+cd ${SOURCE_DIR}
 
 mkdir -p ~/.gnupg
 chmod 600 ~/.gnupg
@@ -19,8 +20,8 @@ mkdir -p /home/robc/.gnupg/private-keys-v1.d
 
 echo allow-loopback-pinentry > ~/.gnupg/gpg-agent.conf
 
-gpg --import build/travis/pubring.gpg
-gpg --passphrase "$GPG_PASSPHRASE" --batch --no-tty --yes --pinentry-mode loopback --allow-secret-key-import --import build/travis/secring.gpg
+gpg --import ${SOURCE_DIR}/build/travis/pubring.gpg
+gpg --passphrase-file /tmp/secrets/priv-key --batch --no-tty --yes --pinentry-mode loopback --allow-secret-key-import --import /tmp/secrets/secring.gpg
 
 git remote set-branches --add origin debian-packaging
 git fetch
@@ -72,7 +73,7 @@ do
 
     dch -b -D "$series" --force-distribution -v "${VERSION}-ppa1~${series}1" "New release"
 
-    debuild -p"gpg --passphrase \"$GPG_PASSPHRASE\" --batch --no-tty --yes --pinentry-mode loopback" -d -S -sa -kD5A196C1776BD06C -j8 --lintian-opts --suppress-tags bad-distribution-in-changes-file
+    debuild -p"gpg --passphrase-file /tmp/secrets/priv-key --batch --no-tty --yes --pinentry-mode loopback" -d -S -sa -kD5A196C1776BD06C -j8 --lintian-opts --suppress-tags bad-distribution-in-changes-file
 
     if [[ -z "$TRAVIS_TAG" ]]; then
         echo "No tag build."
@@ -81,5 +82,4 @@ do
         echo "Tag build : $TRAVIS_TAG"
         # dput ppa:rob-caelers/workrave ../workrave_*_source.changes
     fi
-
 done
