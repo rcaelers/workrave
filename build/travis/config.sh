5fcb2cbf080952238ef89c8b769fe70f8a7d11a1
Rob Caelers <rob.caelers@gmail.com>
2019-03-19 17:27:56 +0100

    Fix Travis deployment
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/travis/config.sh
+++ build/travis/config.sh
@@ -1,11 +1,17 @@
-ROOT_DIR=/workspace
-SOURCES_DIR=${ROOT_DIR}/source
+if [ -d /workspace ]; then
+    ROOT_DIR=/workspace
+    SOURCES_DIR=${ROOT_DIR}/source
+
+    ISCC=${ROOT_DIR}/inno/app/ISCC.exe
+else
+    SOURCES_DIR=${TRAVIS_BUILD_DIR}
+fi
+
 DEPLOY_DIR=${SOURCES_DIR}/_deploy
 BUILD_DIR=${SOURCES_DIR}/_dist/build
 
 MINGW_MAKE_RUNTIME=${SOURCES_DIR}/build/travis/mingw-make-runtime.sh
 MINGW_ENV=${SOURCES_DIR}/build/travis/mingw-env
-ISCC=${ROOT_DIR}/inno/app/ISCC.exe
 
 export DEBFULLNAME="Rob Caelers"
 export DEBEMAIL="robc@krandor.org"
