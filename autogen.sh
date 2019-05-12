224ee1a3d97024961f10230fc0b18cee39526b36
Rob Caelers <rob.caelers@gmail.com>
2019-05-12 22:08:33 +0200

    Find autoconf m4 files in /usr/local in Windows build only
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,4 @@
 set -e
 
 autopoint --force
-AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install -I/usr/local/share/aclocal/
+AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
