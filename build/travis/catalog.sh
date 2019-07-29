fdfd95b6bd097c44632cd9608f7424ee1cff8831
Rob Caelers <rob.caelers@gmail.com>
2019-07-29 13:03:18 +0200

    Use different file name for partial jobs catalog file
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/travis/catalog.sh
+++ build/travis/catalog.sh
@@ -39,7 +39,7 @@ parse_arguments $*
 
 # --arg body "`sed -n "/## \[$TRAVIS_TAG\]/,/## \[/{/## \[/b;p}" CHANGELOG.md`"
 
-CATALOG_NAME=${DEPLOY_DIR}/catalog-${TRAVIS_BUILD_NUMBER}.json
+CATALOG_NAME=${DEPLOY_DIR}/job-catalog-${TRAVIS_BUILD_NUMBER}.json
 
 if [ ! -f $CATALOG_NAME ]; then
     jq -n '
