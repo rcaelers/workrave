0a9d767855d5ac1076ce30fb3788aad8dee41094
Rob Caelers <rob.caelers@gmail.com>
2019-07-31 00:02:32 +0200

    Add git log entries to catalog.
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/travis/catalog/main.js
+++ build/travis/catalog/main.js
@@ -19,9 +19,10 @@ const main = async () => {
     const prefix = 'v1.10';
     const accessKeyId = 'travis';
     const secretAccessKey = getEnv('SNAPSHOTS_SECRET_ACCESS_KEY');
+    const gitRoot = getEnv('TRAVIS_BUILD_DIR');
 
     storage = new S3Store(bucket, accessKeyId, secretAccessKey);
-    let catalog = new Catalog(storage, prefix);
+    let catalog = new Catalog(storage, gitRoot, prefix);
     await catalog.load();
     await catalog.process();
     await catalog.save();
