f5caad3422c480d49b2f4b9cae67f71fffa98c9c
Rob Caelers <rob.caelers@gmail.com>
2019-08-14 16:03:23 +0200

    Properly terminate running Workrave during installation.
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- frontend/gtkmm/win32/setup/src/zapper.c
+++ frontend/gtkmm/win32/setup/src/zapper.c
@@ -39,7 +39,9 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, long lParam)
 
     buf[ n ] = '\0';
 
-    if( strcmp( buf, "EggSmClientWindow" ) )
+    if( (strcmp( buf, "EggSmClientWindow" ) != 0 ) &&
+        (strcmp( buf, "WorkraveTrayObserver" ) != 0 ) &&
+        (strcmp( buf, "HarpoonNotificationWindow" ) != 0 )
         goto cleanup;
 
     if( !GetWindowThreadProcessId( hwnd, &processid )
