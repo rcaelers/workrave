82ae5086538330b95c52d5133a157b075a241133
Rob Caelers <rob.caelers@gmail.com>
2020-06-06 20:52:32 +0200

    Enable Github CI
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- build/ci/build.sh
+++ build/ci/build.sh
@@ -1,14 +1,113 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/config.sh
 
+CMAKE_FLAGS=()
+CMAKE_FLAGS64=()
+MAKE_FLAGS=()
+
+build()
+{
+  config=$1
+  cmake_args=("${!2}")
+
+  mkdir -p ${BUILD_DIR}/${config}
+  mkdir -p ${OUTPUT_DIR}/${config}
+
+  cd ${BUILD_DIR}/${config}
+
+  cmake ${SOURCES_DIR} -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/${config} ${cmake_args[@]}
+
+  make ${MAKE_FLAGS[@]} VERBOSE=1
+  make ${MAKE_FLAGS[@]} install VERBOSE=1
+}
+
+if [[ ${CONF_ENABLE} ]]; then
+    for i in ${CONF_ENABLE//,/ }
+    do
+        CMAKE_FLAGS+=("-DWITH_$i=ON")
+        CMAKE_FLAGS64+=("-DWITH_$i=ON")
+    done
+fi
+
+if [[ ${CONF_DISABLE} ]]; then
+    for i in ${CONF_DISABLE//,/ }
+    do
+        CMAKE_FLAGS+=("-DWITH_$i=OFF")
+        CMAKE_FLAGS64+=("-DWITH_$i=OFF")
+    done
+fi
+
+CMAKE_FLAGS+=("-DWITH_UI=${WITH_UI}")
+
+if [[ $COMPILER = 'gcc' ]] ; then
+    CMAKE_FLAGS+=("-DCMAKE_CXX_COMPILER=g++")
+    CMAKE_FLAGS64+=("-DCMAKE_CXX_COMPILER=g++")
+    CMAKE_FLAGS+=("-DCMAKE_C_COMPILER=gcc")
+    CMAKE_FLAGS64+=("-DCMAKE_C_COMPILER=gcc")
+elif [[ $COMPILER = 'clang' ]] ; then
+    CMAKE_FLAGS+=("-DCMAKE_CXX_COMPILER=clang++-9")
+    CMAKE_FLAGS64+=("-DCMAKE_CXX_COMPILER=clang++-9")
+    CMAKE_FLAGS+=("-DCMAKE_C_COMPILER=clang-9")
+    CMAKE_FLAGS64+=("-DCMAKE_C_COMPILER=clang-9")
+fi
+
+CMAKE_FLAGS+=("-DCMAKE_BUILD_TYPE=$CONFIGURATION")
+
+if [ "$(uname)" == "Darwin" ]; then
+    CMAKE_FLAGS+=("-DCMAKE_PREFIX_PATH=$(brew --prefix qt5)")
+fi
+
 case "$DOCKER_IMAGE" in
-    mingw-gtk2)
-        ${CI_DIR}/build-mingw.sh
+    mingw-qt5)
+        CMAKE_FLAGS+=("-DCMAKE_TOOLCHAIN_FILE=${SOURCES_DIR}/build/cmake/mingw64.cmake")
+        CMAKE_FLAGS+=("-DPREBUILT_PATH=${WORKSPACE}/prebuilt")
         ;;
 
-    *)
-        ${CI_DIR}/build-linux.sh
+    mingw-gtk*)
+
+        case "$CONFIG" in
+            vs)
+                CMAKE_FLAGS+=("-DCMAKE_TOOLCHAIN_FILE=${SOURCES_DIR}/build/cmake/mingw32.cmake")
+                CMAKE_FLAGS+=("-DPREBUILT_PATH=${WORKSPACE}/prebuilt")
+                ;;
+
+            *)
+                CMAKE_FLAGS+=("-DCMAKE_TOOLCHAIN_FILE=${SOURCES_DIR}/build/cmake/mingw32.cmake")
+                CMAKE_FLAGS+=("-DPREBUILT_PATH=${OUTPUT_DIR}/.64")
+
+                CMAKE_FLAGS64+=("-DCMAKE_TOOLCHAIN_FILE=${SOURCES_DIR}/build/cmake/mingw64.cmake")
+                CMAKE_FLAGS64+=("-DWITH_UI=None")
+                CMAKE_FLAGS64+=("-DCMAKE_BUILD_TYPE=Release")
+
+                build ".64" CMAKE_FLAGS64[@]
+                ;;
+        esac
         ;;
 esac
+
+build "" CMAKE_FLAGS[@]
+
+mkdir ${DEPLOY_DIR}
+
+EXTRA=
+CONFIG=release
+if [ $CONFIGURATION == "Debug" ]; then
+    EXTRA="-Debug"
+    CONFIG="debug"
+fi
+
+if [[ -e ${OUTPUT_DIR}/mysetup.exe ]]; then
+    if [[ -z "$WORKRAVE_TAG" ]]; then
+        echo "No tag build."
+        filename=workrave-win32-${WORKRAVE_LONG_GIT_VERSION}-${WORKRAVE_BUILD_DATE}${EXTRA}.exe
+    else
+        echo "Tag build : $WORKRAVE_TAG"
+        filename=workrave-win32-${WORKRAVE_VERSION}${EXTRA}.exe
+    fi
+
+    cp ${OUTPUT_DIR}/mysetup.exe ${DEPLOY_DIR}/${filename}
+
+    ${SOURCES_DIR}/build/travis/catalog.sh -f ${filename} -k installer -c $CONFIG -p windows
+fi
