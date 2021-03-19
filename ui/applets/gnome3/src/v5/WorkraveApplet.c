3b52332cb494f9b71fce4a47bcc731d7637e8ced
Rob Caelers <rob.caelers@gmail.com>
2021-03-19 18:24:30 +0100

    Allow disable status icon in all applets

--- ui/applets/gnome3/src/v5/WorkraveApplet.c
+++ ui/applets/gnome3/src/v5/WorkraveApplet.c
@@ -374,7 +374,7 @@ workrave_applet_fill(WorkraveApplet *applet)
   g_signal_connect(G_OBJECT(applet->priv->timerbox_control), "alive-changed", G_CALLBACK(on_alive_changed), applet);
 
   workrave_timerbox_control_set_tray_icon_visible_when_not_running(applet->priv->timerbox_control, TRUE);
-  workrave_timerbox_control_set_tray_icon_mode(applet->priv->timerbox_control, WORKRAVE_TIMERBOX_CONTROL_TRAY_ICON_MODE_ALWAYS);
+  workrave_timerbox_control_set_tray_icon_mode(applet->priv->timerbox_control, WORKRAVE_TIMERBOX_CONTROL_TRAY_ICON_MODE_FOLLOW);
 
   applet->priv->action_group = g_simple_action_group_new();
   g_action_map_add_action_entries(G_ACTION_MAP(applet->priv->action_group), menu_actions, G_N_ELEMENTS(menu_actions), applet);
