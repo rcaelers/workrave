e790cbce10dd8febf317b400db4f3c8227d66d06
Rob Caelers <rob.caelers@gmail.com>
2021-06-19 23:59:02 +0200

    Fix Windows build

--- ui/apps/qt5/dist/PackagingMacOS.cmake
+++ ui/apps/qt5/dist/PackagingMacOS.cmake
@@ -1,18 +1,3 @@
-get_target_property(_qmake_executable Qt${QT_VERSION_MAJOR}::qmake IMPORTED_LOCATION)
-get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
-
-mark_as_advanced(MACDEPLOYQT_EXECUTABLE)
-
-function(macdeployqt target)
-    add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${MACDEPLOYQT_EXECUTABLE}"
-            \"$<TARGET_FILE_DIR:${target}>/../..\"
-            -always-overwrite
-        COMMENT "Deploying Qt."
-    )
-endfunction()
-
 configure_file(${CMAKE_SOURCE_DIR}/ui/apps/qt5/dist/macos/Info.plist.in ${CMAKE_BINARY_DIR}/Info.plist)
 
 list(APPEND CPACK_GENERATOR "DragNDrop")
