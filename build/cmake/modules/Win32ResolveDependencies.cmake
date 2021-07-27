b8596ffa60c76e9f9c35b95c496865d7d2a648b0
Rob Caelers <rob.caelers@gmail.com>
2021-07-27 21:03:25 +0200

    Fix dependency resolving on Windows

--- build/cmake/modules/Win32ResolveDependencies.cmake
+++ build/cmake/modules/Win32ResolveDependencies.cmake
@@ -3,20 +3,19 @@ function(resolve_dependencies target dependencies_out resolved_dependencies_out 
   set(new_dependencies)
 
   execute_process(COMMAND ${CMAKE_OBJDUMP} "-p" ${target} OUTPUT_VARIABLE objdump_output)
+
   string(REGEX REPLACE ";" "\\\\;" lines "${objdump_output}")
   string(REGEX REPLACE "\n" ";" lines "${lines}")
 
   foreach(line ${lines})
-    set(regex "^\t*DLL Name: (.*\\.[Dd][Ll][Ll])$")
+    set(regex "^[ \t]*DLL Name: (.*\\.[Dd][Ll][Ll])$")
 
     if("${line}" MATCHES "${regex}")
       string(REGEX REPLACE ${regex} "\\1" item "${line}")
 
       list(FIND ${dependencies_out} ${item} exists)
       if(exists EQUAL -1)
-
         list(APPEND ${dependencies_out} ${item})
-
         set(path "path-NOTFOUND")
         find_file(path "${item}" PATHS ${dependencies_search_dirs} NO_DEFAULT_PATH)
 
