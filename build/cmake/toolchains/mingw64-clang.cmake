fafa3039855b8d3a9738ef0543a4b01c91f82c0c
Rob Caelers <rob.caelers@gmail.com>
2022-01-14 21:57:37 +0100

    Add MinGW devcontainer

--- build/cmake/toolchains/mingw64-clang.cmake
+++ build/cmake/toolchains/mingw64-clang.cmake
@@ -1,5 +1,5 @@
 set(SYS_ROOT_PREFIX "")
-
+set(GCC_VERSION 11.2.1)
 set(TOOLCHAIN_ROOT "/usr/x86_64-w64-mingw32")
 set(SYS_ROOT "${SYS_ROOT_PREFIX}/usr/x86_64-w64-mingw32/sys-root/mingw/")
 
@@ -10,25 +10,43 @@ set(ZLIB_ROOT ${SYS_ROOT}/lib)
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_VERSION 10)
 
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-clang)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-clang++)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_RC_COMPILER llvm-rc)
+set(CMAKE_ASM_MASM_COMPILER uasm)
 
-SET (CMAKE_C_FLAGS              "-fuse-ld=lld")
-SET (CMAKE_CXX_FLAGS            ${CMAKE_C_FLAGS})
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_ASM_COMPILER_TARGET x86_64-w64-mingw32)
 
-set (WINE wine)
-set (ISCC "/workspace/inno/app/ISCC.exe")
+set(CMAKE_C_FLAGS              "-fuse-ld=lld")
+set(CMAKE_CXX_FLAGS            ${CMAKE_C_FLAGS})
 
+set(CMAKE_SYSROOT ${SYS_ROOT})
 set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_ROOT} ${SYS_ROOT})
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-include_directories(${TOOLCHAIN_ROOT}/include)
-include_directories(${SYS_ROOT}/include)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(ENV{PKG_CONFIG_LIBDIR} "${SYS_ROOT}/lib/pkgconfig")
 set(ENV{PKG_CONFIG_PATH} "")
 set(ENV{PKG_CONFIG_SYSROOT_DIR} "${SYS_ROOT_PREFIX}")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--sysroot=${SYS_ROOT} -L/usr/lib/gcc/${CMAKE_C_COMPILER_TARGET}/${GCC_VERSION}")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
+
+set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES
+    /usr/lib64/clang/13.0.0/include
+    ${SYS_ROOT}/include/c++/
+    ${SYS_ROOT}/include/c++/${CMAKE_CXX_COMPILER_TARGET}
+    ${SYS_ROOT}/${CMAKE_C_COMPILER_TARGET}/include
+    ${SYS_ROOT}/include
+)
+
+set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_C_STANDARD_INCLUDE_DIRECTORIES})
+
+set (WINE wine)
+set (ISCC "/workspace/inno/app/ISCC.exe")
