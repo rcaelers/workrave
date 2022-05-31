f1ad14c15e18a84644c02a336a3ff8489ce2e100
Rob Caelers <rob.caelers@gmail.com>
2022-05-31 19:59:35 +0200

    Fix Windows build

--- build/cmake/toolchains/mingw32-clang.cmake
+++ build/cmake/toolchains/mingw32-clang.cmake
@@ -1,5 +1,5 @@
 set(SYS_ROOT_PREFIX "")
-set(GCC_VERSION 12.0.1)
+set(GCC_VERSION 12.1.1)
 set(TOOLCHAIN_ROOT "/usr/i686-w64-mingw32")
 set(SYS_ROOT "${SYS_ROOT_PREFIX}/usr/i686-w64-mingw32/sys-root/mingw/")
 
