d3b69be80f47c9fd73701980ddc50649cbd56881
Rob Caelers <rob.caelers@gmail.com>
2022-08-04 15:49:48 +0200

    Appcast generation

--- tools/appcast/src/generator.ts
+++ tools/appcast/src/generator.ts
@@ -12,6 +12,14 @@ class Generator {
   params: any;
   catalog: any;
 
+  tag_to_version(tag: any) {
+    return tag
+      .replace(/_([0-9])/g, '.$1')
+      .replace(/-[0-9]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/^v/g, '');
+  }
+
   constructor(catalog: any, params: any) {
     this.catalog = catalog;
     this.params = params;
@@ -21,15 +29,29 @@ class Generator {
         autoescape: false,
         watch: false
       })
-      .addFilter('data_format', function (date: string, format: string) {
+      .addFilter('data_format', (date: string, format: string) => {
         return moment(date).format(format);
       })
-      .addFilter('channel', function (str: string) {
-        const pre = semver.prerelease(str);
+      .addFilter('data_format_from_unix', (date: string, format: string) => {
+        return moment.unix(+date).format(format);
+      })
+      .addFilter('channel', (item: any) => {
+        const version = this.tag_to_version(item.tag);
+        const increment = item.increment;
+
+        if (increment !== '0' && increment !== '') {
+          return 'dev';
+        }
+
+        const pre = semver.prerelease(version);
         if (pre) {
           return pre[0];
         }
         return '';
+      })
+
+      .addFilter('version', (item: any) => {
+        return this.tag_to_version(item.tag);
       });
   }
 
