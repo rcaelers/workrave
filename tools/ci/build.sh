083c4d59c5340000e8e59bb887011fe1254eea97
Rob Caelers <rob.caelers@gmail.com>
2022-08-14 22:49:55 +0200

    Don't create deplot artifact when building lcoally

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -216,16 +216,19 @@ if [[ $MSYSTEM == "CLANG64" ]]; then
 
     if [[ -e ${OUTPUT_DIR}/workrave-installer.exe ]]; then
 
-        deployFilename=${baseFilename}.tar.zst
+        if [[ $WORKRAVE_ENV != "local-windows-msys2" ]]; then
 
-        issdir=${BUILD_DIR}/${config}/ui/app/toolkits/gtkmm/dist/windows/
-        prefix="$(grep ^LicenseFile ${issdir}/setup.iss | sed -e 's/LicenseFile=\(.*\)/\1/' | rev | cut -d\\ -f2- | rev)\\"
-        for iss in ${issdir}/*.iss; do
-            cat $iss | sed -e "s|${prefix//\\/\\\\}||" >${OUTPUT_DIR}/$(basename $iss)
-        done
+            deployFilename=${baseFilename}.tar.zst
 
-        tar cavf ${DEPLOY_DIR}/${deployFilename} -C $(dirname ${OUTPUT_DIR}) --exclude "**/workrave-installer.exe" ${OUTPUT_DIR}
-        ${SCRIPTS_DIR}/ci/artifact.sh -f ${deployFilename} -k deploy -c $CONFIG -p windows
+            issdir=${BUILD_DIR}/${config}/ui/app/toolkits/gtkmm/dist/windows/
+            prefix="$(grep ^LicenseFile ${issdir}/setup.iss | sed -e 's/LicenseFile=\(.*\)/\1/' | rev | cut -d\\ -f2- | rev)\\"
+            for iss in ${issdir}/*.iss; do
+                cat $iss | sed -e "s|${prefix//\\/\\\\}||" >${OUTPUT_DIR}/$(basename $iss)
+            done
+
+            tar cavf ${DEPLOY_DIR}/${deployFilename} -C $(dirname ${OUTPUT_DIR}) --exclude "**/workrave-installer.exe" ${OUTPUT_DIR}
+            ${SCRIPTS_DIR}/ci/artifact.sh -f ${deployFilename} -k deploy -c $CONFIG -p windows
+        fi
 
         filename=${baseFilename}.exe
         symbolsFilename=${baseFilename}.sym
