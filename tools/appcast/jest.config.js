41367eaa08b4ffea77e8983f7eb357b9113544fd
Rob Caelers <rob.caelers@gmail.com>
2022-09-06 18:59:00 +0200

    Remove appcast CI tool

--- tools/appcast/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src'],
-};
--- tools/appcast/src/main.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import CLI from './cli.js';
-
-const main: CLI = new CLI();
-main.run();
