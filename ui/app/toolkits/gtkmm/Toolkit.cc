4af97a5aefee98ba14413bff4e5df0476bb754e3
Rob Caelers <rob.caelers@gmail.com>
2026-09-24 20:16:32 +0200

    fix: set application name for Workrave in Toolkit initialization

--- ui/app/toolkits/gtkmm/Toolkit.cc
+++ ui/app/toolkits/gtkmm/Toolkit.cc
@@ -73,6 +73,7 @@ Toolkit::init(std::shared_ptr<IApplicationContext> app)
   this->app = app;
 
   Glib::set_prgname("org.workrave.Workrave");
+  Glib::set_application_name("Workrave");
   gapp = Gtk::Application::create(argc, argv, "org.workrave.Workrave");
   init_css();
   gtk_icon_theme_add_resource_path(gtk_icon_theme_get_default(), "/workrave/icons/scalable");
